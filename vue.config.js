d898ca90357d0af632f7cbff9d27d2b7cc561fb0
饶盼 <raopan2021@outlook.com>
2022-12-29 14:43:14 +0800

    调试部署

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/shuo_xue' //这里写gitee pages里面仓库的名称
+  : '/'
 module.exports = {
   chainWebpack: config => {
     config.module.rule('md')
@@ -10,5 +19,92 @@ module.exports = {
       .options({
         raw: true
       })
+  }
+  // 项目部署的基础路径
+  // 我们默认假设你的应用将会部署在域名的根部，
+  // 比如 https://www.my-app.com/
+  // 如果你的应用时部署在一个子路径下，那么你需要在这里
+  // 指定子路径。比如，如果你的应用部署在
+  // https://www.foobar.com/my-app/
+  // 那么将这个值改为 `/my-app/`
+  publicPath: BASE_URL,　　/*这个是我存放在github在线预览的Reader项目*/
+
+  // 将构建好的文件输出到哪里（或者说将编译的文件）
+  outputDir: 'dist',
+
+  // 放置静态资源的地方 (js/css/img/font/...)
+  assetsDir: '',
+
+  // 用于多页配置，默认是 undefined
+  pages: {
+    index: {
+      // 入口文件
+      entry: 'src/main.js',　　/*这个是根入口文件*/
+      // 模板文件
+      template: 'public/index.html',
+      // 输出文件
+      filename: 'index.html',
+      // 页面title
+      title: 'Index Page'
+    },
+    // 简写格式
+    // 模板文件默认是 `public/subpage.html`
+    // 如果不存在，就是 `public/index.html`.
+    // 输出文件默认是 `subpage.html`.
+    subpage: 'src/main.js'　　　　/*注意这个是*/
+  },
+
+  // 是否在保存的时候使用 `eslint-loader` 进行检查。
+  // 有效的值：`ture` | `false` | `"error"`
+  // 当设置为 `"error"` 时，检查出的错误会触发编译失败。
+  lintOnSave: true,
+
+  // 是否为生产环境构建生成 source map？
+  productionSourceMap: true,
+
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
+      .set('_conf', resolve('config'))
+      .set('_p', resolve('public'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
+  // devServer: {
+  //   proxy: 'localhost:3000'
+  // }
+  //less版本过高需要添加
+  css: {
+    loaderOptions: {
+      less: {
+          javascriptEnabled: true,
+      }
+    }
+  },
+
+  // 配置 webpack-dev-server 行为。
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli/cli-service.md#配置代理
+    proxy: {
+          '/api': {
+                target: 'http://localhost:8880',
+                changeOrigin: true,
+                secure: false,
+                // ws: true,
+                pathRewrite: {
+                    '^/api': '/static/mock'   // 请求数据路径别名,这里是注意将static/mock放入public文件夹
+                }
+          }
+   },
+   before: app => {}
+  },
+
+  // 三方插件的选项
+  pluginOptions: {
+    // ...
   }
 }
