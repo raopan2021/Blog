fc6ba26a5545e412eab96ce22c2ab1cb964deb4b
SYMF <raopan2021@outlook.com>
2023-01-17 15:12:20 +0800

    数组、字符串方法；测试同时推送到gitee和github

---
+++ src/views/resume/AboutView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="about">
+    <VueScrollProgress></VueScrollProgress>
+    <header-view></header-view>
+
+    <div class="doc-main">
+
+      <!-- <div class="markdown-body"> -->
+        <!-- 文章详情页面 -->
+        <div class="details">
+          <readme-view class="markdown-body" v-highlight></readme-view>
+        </div>
+      <!-- </div> -->
+    </div>
+
+      <el-backtop>
+        <div class="up">UP</div>
+      </el-backtop>
+
+      <!-- <audio-view></audio-view> -->
+  </div>
+</template>
+
+<script>
+// 加载樱花飘落的特效
+// import "@/utils/specialEfficiency/sakura.js";
+
+import HeaderView from "@/components/Header.vue";
+import ReadmeView from '@/../README.md';
+// import AudioView from '@/components/AudioView.vue';
+
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    HeaderView,
+    ReadmeView,
+    // AudioView
+  },
+  mounted () {
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss"
+</style>
